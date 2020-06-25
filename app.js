6e86e3f31b380d079798f9e32e298b616efb67fd
tomasz kasprowicz <tomaszkasprowicz24@gmail.com>
2020-06-25 14:45:56 +0200

    error handling

--- app.js
+++ app.js
@@ -24,10 +24,16 @@ document.addEventListener("DOMContentLoaded", () => {
   function showPosition(position) {
     const lat = position.coords.latitude;
     const long = position.coords.longitude;
-    weather.apiByGeo(lat, long).then((data) => {
-      ui.draw(data);
-    });
-    weather.apiForecast(lat, long).then((data) => ui.drawForecast(data));
+    weather
+      .apiByGeo(lat, long)
+      .then((data) => {
+        ui.draw(data);
+      })
+      .catch((err) => err);
+    weather
+      .apiForecast(lat, long)
+      .then((data) => ui.drawForecast(data))
+      .catch((err) => err);
   }
   getLocation();
 });
@@ -36,12 +42,24 @@ button.addEventListener("click", changeUnits);
 function changeUnits() {
   const city = document.querySelector(".city").innerText;
   if (button.innerText == "F") {
-    weather.apiByCityName(city, "imperial").then((data) => ui.draw(data, "f"));
-    weather.apiForecastbyCityName(city, 'imperial').then(data=> ui.drawForecast(data,'f'))
+    weather
+      .apiByCityName(city, "imperial")
+      .then((data) => ui.draw(data, "f"))
+      .catch((err) => err);
+    weather
+      .apiForecastbyCityName(city, "imperial")
+      .then((data) => ui.drawForecast(data, "f"))
+      .catch((err) => err);
     button.innerText = "C";
   } else {
-    weather.apiByCityName(city).then((data) => ui.draw(data));
-    weather.apiForecastbyCityName(city, 'metric').then(data=> ui.drawForecast(data,'c'))
+    weather
+      .apiByCityName(city)
+      .then((data) => ui.draw(data))
+      .catch((err) => err);
+    weather
+      .apiForecastbyCityName(city, "metric")
+      .then((data) => ui.drawForecast(data, "c"))
+      .catch((err) => err);
     button.innerText = "F";
   }
 }
@@ -55,49 +73,51 @@ function slideMenu() {
 searchBtn.addEventListener("click", changeCity);
 
 function changeCity() {
-  weather.apiByCityName(inputText.value).then((data) => ui.draw(data));
-  weather
-    .apiForecastbyCityName(inputText.value)
-    .then((data) => ui.drawForecast(data));
-  Ui.clearInput();
+  if (inputText.value !== "") {
+    weather
+      .apiByCityName(inputText.value)
+      .then((data) => {
+        if (data.name !== undefined) ui.draw(data);
+        // else throw new Error('not working')
+      })
+      .catch((err) => console.log(err));
+    weather
+      .apiForecastbyCityName(inputText.value)
+      .then((data) => ui.drawForecast(data))
+      .catch((err) => err);
+    Ui.clearInput();
+  }
 }
 
-var mySwiper = new Swiper('.swiper-container', {
-  direction: 'horizontal',
+const mySwiper = new Swiper(".swiper-container", {
+  direction: "horizontal",
   spaceBetween: 20,
   slidesPerView: 8,
   grabCursor: true,
   breakpoints: {
     320: {
       slidesPerView: 2,
-      
     },
     480: {
       slidesPerView: 3,
-      
     },
     640: {
       slidesPerView: 4,
     },
     800: {
       slidesPerView: 5,
-      
     },
     960: {
       slidesPerView: 6,
-      
     },
     1120: {
       slidesPerView: 7,
-      
     },
     1280: {
       slidesPerView: 8,
-      
     },
     1400: {
       slidesPerView: 9,
-      
-    }
-  }
-})
+    },
+  },
+});
